de54eec5c729b33defa48ef1f4fc1252399ae227
maxsynnott <maxryansynnott@gmail.com>
2020-09-06 17:55:59 +0200

    initial urql setup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
+
+import { Provider, Client, dedupExchange, fetchExchange } from "urql";
+import { cacheExchange } from "@urql/exchange-graphcache";
+
+const cache = cacheExchange({});
+
+const client = new Client({
+	url: "http://localhost:4000",
+	exchanges: [cache, dedupExchange, fetchExchange],
+});
 
 ReactDOM.render(
-	<React.StrictMode>
+	<Provider value={client}>
 		<App />
-	</React.StrictMode>,
+	</Provider>,
 	document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
