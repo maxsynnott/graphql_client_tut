b573bd3e5e5f8afcd1f128e8b34bc72e68e46bfc
maxsynnott <maxryansynnott@gmail.com>
2020-09-06 18:45:59 +0200

    CreateLink in (awaiting authentication logic)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "../styles/App.css";
 import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
 
-const App = () => <LinkList />;
+const App = () => <CreateLink />;
 
 export default App;
